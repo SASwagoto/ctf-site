8f89c63e01e9ae754e6d7e5f60e1fd3b90b348f0
Swagoto <saswagoto@gmail.com>
2023-12-04 04:57:06 +0600

    ok

---
+++ assets/js/animation.js
@@ -0,0 +1,18 @@
+let text = baffle('.baffle-text');
+text.set({
+    characters: [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+    ],
+    speed: 50
+});
+text.start();
+text.reveal(4000, 800);
+function revealAgain(){
+    text.start();
+    text.reveal(4000, 800);
+}
+

a0b384b969f00c4b9441b93aa183963e62483f03
SASwagoto <saswagoto@gmail.com>
2023-12-06 17:20:25 +0600

    ok

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,31 @@ function handleTickInit(tick) {
       tick.value = value;
     };
   }
+
+
+  $('.platinum_logo').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 1000,
+    centerMode: true,
+  });
+
+  $('.gold_logo').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 1500,
+    centerMode: true,
+  });
+
+  $('.silver_logo').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 300,
+    centerMode: true
+  });
